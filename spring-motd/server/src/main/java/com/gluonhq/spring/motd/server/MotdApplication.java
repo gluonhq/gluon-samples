dcef52f9ef44a0d6018c83778048e9ee48b28b47
erwin.morrhey <erwin@lodgon.com>
2017-05-23 21:06:58 +0200

    Spring example updated to use new GCL enterprise client

--- spring-motd/server/src/main/java/com/gluonhq/spring/motd/server/MotdApplication.java
+++ spring-motd/server/src/main/java/com/gluonhq/spring/motd/server/MotdApplication.java
@@ -26,9 +26,15 @@
  */
 package com.gluonhq.spring.motd.server;
 
+import com.gluonhq.cloudlink.client.enterprise.CloudLinkClient;
+import com.gluonhq.cloudlink.client.enterprise.CloudLinkConfig;
+import com.gluonhq.cloudlink.client.enterprise.feign.FeignCloudLinkClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class MotdApplication extends SpringBootServletInitializer {
@@ -37,6 +43,23 @@ public class MotdApplication extends SpringBootServletInitializer {
         new SpringApplicationBuilder(MotdApplication.class)
                 .application()
                 .run(args);
+    }
+
+}
+
+@Configuration
+class MotdConfig {
+
+    @Value("${gcl.endpoint}")
+    private String gcl_endpoint;
+
+    @Value("${gcl.key}")
+    private String gcl_key;
+
+    @Bean
+    public CloudLinkClient cloudLinkClient() {
+        return new FeignCloudLinkClient( new CloudLinkConfig( gcl_endpoint, gcl_key ));
+
     }
 
 }
