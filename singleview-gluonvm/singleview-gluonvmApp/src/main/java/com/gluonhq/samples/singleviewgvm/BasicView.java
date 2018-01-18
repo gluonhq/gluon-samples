f43744a5007a3f20da49a89cfda1390089313ffa
Joeri Sykora <joeri.sykora@gluonhq.com>
2018-01-18 14:09:55 +0100

    add basic sample for gluon vm

---
+++ singleview-gluonvm/singleview-gluonvmApp/src/main/java/com/gluonhq/samples/singleviewgvm/BasicView.java
@@ -0,0 +1,42 @@
+package com.gluonhq.samples.singleviewgvm;
+
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.control.Icon;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BasicView extends View {
+
+    public BasicView(String name) {
+        super(name);
+
+        Label label = new Label("Hello JavaFX World!");
+
+        Button button = new Button("Change the World!");
+        button.setGraphic(new Icon(MaterialDesignIcon.LANGUAGE));
+        button.setOnAction(e -> {
+            String numbers = IntStream.range(1, 11).boxed().map(String::valueOf).collect(Collectors.joining(", "));
+            label.setText(numbers);
+        });
+
+        VBox controls = new VBox(15.0, label, button);
+        controls.setAlignment(Pos.CENTER);
+
+        setCenter(controls);
+    }
+
+    @Override
+    protected void updateAppBar(AppBar appBar) {
+        appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> System.out.println("Menu")));
+        appBar.setTitleText("Basic View");
+        appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e -> System.out.println("Search")));
+    }
+
+}
