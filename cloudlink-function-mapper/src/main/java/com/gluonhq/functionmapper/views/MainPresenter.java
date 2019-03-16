6844efd9b69a0052d32b3411b350ed79bbaeebbb
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-03-16 23:34:06 +0530

    Improvements to function mapper demo

--- cloudlink-function-mapper/src/main/java/com/gluonhq/functionmapper/views/MainPresenter.java
+++ cloudlink-function-mapper/src/main/java/com/gluonhq/functionmapper/views/MainPresenter.java
@@ -48,7 +48,7 @@ import java.util.List;
 
 public class MainPresenter extends GluonPresenter<FunctionMapper> {
 
-    private static final List<String> TAGS = Arrays.asList(new String[] { "gluon", "gluon-mobile", "javafxports", "javafx", "scenebuilder" });
+    private static final List<String> TAGS = Arrays.asList("gluon", "gluon-mobile", "javafxports", "javafx", "scenebuilder");
   
     @Inject
     private RemoteService remoteService;
@@ -148,5 +148,4 @@ public class MainPresenter extends GluonPresenter<FunctionMapper> {
             }
         });
     }
-    
 }
