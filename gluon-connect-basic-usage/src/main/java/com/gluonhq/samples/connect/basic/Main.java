1203732dfc7fca34d28ea290aba624d5f0c3eb37
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2020-04-14 16:33:19 +0530

    Use NavigationDrawer from MobileApplication

--- gluon-connect-basic-usage/src/main/java/com/gluonhq/samples/connect/basic/Main.java
+++ gluon-connect-basic-usage/src/main/java/com/gluonhq/samples/connect/basic/Main.java
@@ -28,7 +28,6 @@ package com.gluonhq.samples.connect.basic;
 
 import com.gluonhq.charm.glisten.application.MobileApplication;
 import com.gluonhq.charm.glisten.control.NavigationDrawer;
-import com.gluonhq.charm.glisten.layout.layer.SidePopupView;
 import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import com.gluonhq.charm.glisten.visual.Swatch;
 
@@ -40,26 +39,23 @@ public class Main extends MobileApplication {
 
     public static final String LIST_VIEW = HOME_VIEW;
     public static final String OBJECT_VIEW = "ObjectView";
-    public static final String MENU_LAYER = "SideMenu";
 
     @Override
     public void init() {
         addViewFactory(LIST_VIEW, () -> new BasicListView());
         addViewFactory(OBJECT_VIEW, () -> new BasicObjectView());
 
-        NavigationDrawer navigationDrawer = new NavigationDrawer();
+        NavigationDrawer navigationDrawer = getDrawer();
         NavigationDrawer.Item listItem = new NavigationDrawer.Item("List Viewer", MaterialDesignIcon.VIEW_LIST.graphic());
         NavigationDrawer.Item objectItem = new NavigationDrawer.Item("Object Viewer", MaterialDesignIcon.INSERT_DRIVE_FILE.graphic());
         navigationDrawer.getItems().addAll(listItem, objectItem);
         navigationDrawer.selectedItemProperty().addListener((obs, oldItem, newItem) -> {
-            hideLayer(MENU_LAYER);
             if (newItem.equals(listItem)) {
                 switchView(LIST_VIEW);
             } else if (newItem.equals(objectItem)) {
                 switchView(OBJECT_VIEW);
             }
         });
-        addLayerFactory(MENU_LAYER, () -> new SidePopupView(navigationDrawer));
     }
 
     @Override
