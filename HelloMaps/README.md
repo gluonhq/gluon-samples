4dfd0bed9dfe112a7ae4dbed12be51b7338294f2
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-04-28 11:23:51 +0530

    Add HelloMaps sample

---
+++ HelloMaps/README.md
@@ -0,0 +1,55 @@
+# HelloMaps
+
+A [Gluon Client](https://docs.gluonhq.com/client/) sample which makes use of [Gluon Maps](https://gluonhq.com/labs/maps/)
+to show a pinned location on a global map.
+
+## Quick Instructions
+
+We use [Gluon Client](https://docs.gluonhq.com/) to build a native image for platforms including desktop, android and iOS.
+Please follow the Gluon Client prerequisites as stated [here](https://docs.gluonhq.com/#_requirements).
+
+### Desktop
+
+Run the application using:
+
+    mvn javafx:run
+
+Build a native image using:
+
+    mvn client:build
+
+Run the native image app:
+
+    mvn client:run
+
+### Android
+
+Build a native image for Android using:
+
+    mvn client:build -Pandroid
+
+Package the native image as an 'apk' file:
+
+    mvn client:package -Pandroid
+
+Install it on a connected android device:
+
+    mvn client:install -Pandroid
+
+Run the installed app on a connected android device:
+
+    mvn client:run -Pandroid
+
+### iOS
+
+Build a native image for iOS using:
+
+    mvn client:build -Pios
+
+Install and run the native image on a connected iOS device:
+
+    mvn client:run -Pios
+
+Create an IPA file (for submission to TestFlight or App Store):
+
+    mvn client:package -Pios
---
+++ HelloMaps/src/main/java/com/gluonhq/PoiLayer.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2021, Gluon
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Gluon, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL GLUON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.gluonhq;
+
+import com.gluonhq.maps.MapLayer;
+import com.gluonhq.maps.MapPoint;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.util.Pair;
+
+/**
+ * A layer that allows to visualise points of interest.
+ */
+public class PoiLayer extends MapLayer {
+
+    private final ObservableList<Pair<MapPoint, Node>> points = FXCollections.observableArrayList();
+
+    public PoiLayer() {
+    }
+
+    public void addPoint(MapPoint p, Node icon) {
+        points.add(new Pair<>(p, icon));
+        this.getChildren().add(icon);
+        this.markDirty();
+    }
+
+    @Override
+    protected void layoutLayer() {
+        for (Pair<MapPoint, Node> candidate : points) {
+            MapPoint point = candidate.getKey();
+            Node icon = candidate.getValue();
+            Point2D mapPoint = getMapPoint(point.getLatitude(), point.getLongitude());
+            icon.setVisible(true);
+            icon.setTranslateX(mapPoint.getX());
+            icon.setTranslateY(mapPoint.getY());
+        }
+    }
+}
