0e1744b9f03e66860210d76ea459076c8c40563e
José Pereda <pereda@eii.uva.es>
2021-10-07 17:53:42 +0200

    Add GoNative sample (#122)
    Co-authored-by: jose.pereda <jose.pereda@gluonhq.com>

---
+++ GoNative/src/main/java/com/gluonhq/samples/gonative/views/AppViewManager.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021, Gluon
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Gluon, any associated website, nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL GLUON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.gluonhq.samples.gonative.views;
+
+import com.gluonhq.charm.glisten.afterburner.AppView;
+import com.gluonhq.charm.glisten.afterburner.AppViewRegistry;
+import com.gluonhq.charm.glisten.afterburner.GluonPresenter;
+import com.gluonhq.charm.glisten.afterburner.Utils;
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.control.Avatar;
+import com.gluonhq.charm.glisten.control.NavigationDrawer;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.scene.image.Image;
+
+import java.util.Locale;
+
+import static com.gluonhq.charm.glisten.afterburner.AppView.Flag.*;
+
+public class AppViewManager {
+
+    private static final AppViewRegistry REGISTRY = new AppViewRegistry();
+
+    public static final AppView MAIN_VIEW = view("Home", MainPresenter.class, MaterialDesignIcon.HOME, SHOW_IN_DRAWER, HOME_VIEW, SKIP_VIEW_STACK);
+    
+    private static AppView view(String title, Class<? extends GluonPresenter<?>> presenterClass, MaterialDesignIcon menuIcon, AppView.Flag... flags ) {
+        return REGISTRY.createView(name(presenterClass), title, presenterClass, menuIcon, flags);
+    }
+
+    private static String name(Class<? extends GluonPresenter<?>> presenterClass) {
+        return presenterClass.getSimpleName().toUpperCase(Locale.ROOT).replace("PRESENTER", "");
+    }
+    
+    public static void registerViewsAndDrawer(MobileApplication app) {
+        for (AppView view : REGISTRY.getViews()) {
+            view.registerView(app);
+        }
+
+        NavigationDrawer.Header header = new NavigationDrawer.Header("Gluon Mobile",
+                "Native Services",
+                new Avatar(21, new Image(AppViewManager.class.getResourceAsStream("/icon.png"))));
+
+        Utils.buildDrawer(app.getDrawer(), header, REGISTRY.getViews()); 
+    }
+}
